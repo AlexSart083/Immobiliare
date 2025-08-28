60e3da0db7462dc10a5c43182baf98b23bdc59c2
AlexSart083 <100191834+AlexSart083@users.noreply.github.com>
2025-08-28 21:28:21 +0200

    Update calcoli.py

--- calcoli.py
+++ calcoli.py
@@ -17,6 +17,7 @@ def calculate_real_estate_investment_improved(params):
     valori_annuali = []
     affitti_lordi_annuali = []
     affitti_netti_annuali = []
+    affitti_netti_senza_mutuo_annuali = []
     rendimenti_annuali = []
     costi_gestione_annuali = []
     costi_mutuo_annuali = []
@@ -44,38 +45,60 @@ def calculate_real_estate_investment_improved(params):
         tasse_affitto = affitto_effettivo * tassazione_decimal
         manutenzione_annua = valore_corrente * manutenzione_decimal
 
-        costi_totali_annui = (
+        # Costi senza mutuo
+        costi_senza_mutuo = (
             costi_assicurazione_correnti
             + costi_gestione_correnti
             + manutenzione_annua
             + tassa_catastale_corrente
             + tasse_affitto
-            + costo_mutuo_anno
         )
+        
+        # Costi totali con mutuo
+        costi_totali_annui = costi_senza_mutuo + costo_mutuo_anno
+        
+        # Affitti netti
         affitto_netto = affitto_effettivo - costi_totali_annui
+        affitto_netto_senza_mutuo = affitto_effettivo - costi_senza_mutuo
+        
         rendimento_annuo = (affitto_netto / params['valore_immobile']) * 100 if params['valore_immobile'] > 0 else 0
 
         valori_annuali.append(valore_corrente)
         affitti_lordi_annuali.append(affitto_corrente)
         affitti_netti_annuali.append(affitto_netto)
+        affitti_netti_senza_mutuo_annuali.append(affitto_netto_senza_mutuo)
         rendimenti_annuali.append(rendimento_annuo)
         costi_gestione_annuali.append(costi_gestione_correnti)
         costi_mutuo_annuali.append(costo_mutuo_anno)
 
+    # Calcoli finali
     valore_finale_nominale = valori_annuali[-1]
     valore_finale_reale = valore_finale_nominale / ((1 + inflazione_decimal) ** params['anni_investimento'])
     totale_affitti_netti = sum(affitti_netti_annuali)
+    totale_affitti_netti_senza_mutuo = sum(affitti_netti_senza_mutuo_annuali)
     totale_costi_mutuo = sum(costi_mutuo_annuali)
     rendimento_medio_annuo = sum(rendimenti_annuali) / len(rendimenti_annuali) if rendimenti_annuali else 0
 
     guadagno_capitale_nominale = valore_finale_nominale - params['valore_immobile']
     guadagno_capitale_reale = valore_finale_reale - params['valore_immobile']
 
+    # Calcolo valore presente degli affitti (reale)
     totale_affitti_netti_reale = 0
-    for anno, affitto_netto_anno in enumerate(affitti_netti_annuali, 1):
-        valore_presente_affitto = affitto_netto_anno / ((1 + inflazione_decimal) ** anno)
+    totale_affitti_netti_senza_mutuo_reale = 0
+    
+    for anno, (affitto_netto_anno, affitto_senza_mutuo_anno) in enumerate(zip(affitti_netti_annuali, affitti_netti_senza_mutuo_annuali), 1):
+        # Fattore di sconto per il valore presente
+        fattore_sconto = (1 + inflazione_decimal) ** anno
+        
+        # Valore presente dell'affitto netto (con mutuo)
+        valore_presente_affitto = affitto_netto_anno / fattore_sconto
         totale_affitti_netti_reale += valore_presente_affitto
+        
+        # Valore presente dell'affitto netto senza mutuo
+        valore_presente_senza_mutuo = affitto_senza_mutuo_anno / fattore_sconto
+        totale_affitti_netti_senza_mutuo_reale += valore_presente_senza_mutuo
 
+    # Rendimenti totali
     rendimento_totale_nominale = (
         totale_affitti_netti + guadagno_capitale_nominale
         - params['commissione_iniziale'] - params['commissione_finale']
@@ -85,16 +108,42 @@ def calculate_real_estate_investment_improved(params):
         - params['commissione_iniziale'] - params['commissione_finale']
     )
 
+    # Rendimenti totali senza mutuo
+    rendimento_totale_senza_mutuo_nominale = (
+        totale_affitti_netti_senza_mutuo + guadagno_capitale_nominale
+        - params['commissione_iniziale'] - params['commissione_finale']
+    )
+    rendimento_totale_senza_mutuo_reale = (
+        totale_affitti_netti_senza_mutuo_reale + guadagno_capitale_reale
+        - params['commissione_iniziale'] - params['commissione_finale']
+    )
+
+    # CAGR calculations
+    investimento_iniziale = params['valore_immobile']
+    capitale_finale_nominale = valore_finale_nominale + totale_affitti_netti - params['commissione_iniziale'] - params['commissione_finale']
+    capitale_finale_reale = valore_finale_reale + totale_affitti_netti_reale - params['commissione_iniziale'] - params['commissione_finale']
+    
     cagr_nominale = (
-        ((valore_finale_nominale + totale_affitti_netti - params['commissione_iniziale'] - params['commissione_finale']) /
-            params['valore_immobile']) ** (1 / params['anni_investimento']) - 1
-    ) if params['valore_immobile'] > 0 else 0
+        (capitale_finale_nominale / investimento_iniziale) ** (1 / params['anni_investimento']) - 1
+    ) if investimento_iniziale > 0 and capitale_finale_nominale > 0 else 0
 
     cagr_reale = (
-        ((valore_finale_reale + totale_affitti_netti_reale - params['commissione_iniziale'] - params['commissione_finale']) /
-            params['valore_immobile']) ** (1 / params['anni_investimento']) - 1
-    ) if params['valore_immobile'] > 0 else 0
+        (capitale_finale_reale / investimento_iniziale) ** (1 / params['anni_investimento']) - 1
+    ) if investimento_iniziale > 0 and capitale_finale_reale > 0 else 0
 
+    # CAGR senza mutuo
+    capitale_finale_senza_mutuo_nominale = valore_finale_nominale + totale_affitti_netti_senza_mutuo - params['commissione_iniziale'] - params['commissione_finale']
+    capitale_finale_senza_mutuo_reale = valore_finale_reale + totale_affitti_netti_senza_mutuo_reale - params['commissione_iniziale'] - params['commissione_finale']
+    
+    cagr_senza_mutuo_nominale = (
+        (capitale_finale_senza_mutuo_nominale / investimento_iniziale) ** (1 / params['anni_investimento']) - 1
+    ) if investimento_iniziale > 0 and capitale_finale_senza_mutuo_nominale > 0 else 0
+
+    cagr_senza_mutuo_reale = (
+        (capitale_finale_senza_mutuo_reale / investimento_iniziale) ** (1 / params['anni_investimento']) - 1
+    ) if investimento_iniziale > 0 and capitale_finale_senza_mutuo_reale > 0 else 0
+
+    # Analisi crescita affitti
     affitto_iniziale = params['affitto_lordo']
     affitto_finale = affitti_lordi_annuali[-1]
     crescita_affitto_totale = ((affitto_finale / affitto_iniziale) - 1) * 100 if affitto_iniziale > 0 else 0
@@ -111,6 +160,7 @@ def calculate_real_estate_investment_improved(params):
         'valori_annuali': valori_annuali,
         'affitti_lordi_annuali': affitti_lordi_annuali,
         'affitti_netti_annuali': affitti_netti_annuali,
+        'affitti_netti_senza_mutuo_annuali': affitti_netti_senza_mutuo_annuali,
         'rendimenti_annuali': rendimenti_annuali,
         'costi_gestione_annuali': costi_gestione_annuali,
         'costi_mutuo_annuali': costi_mutuo_annuali,
@@ -118,14 +168,20 @@ def calculate_real_estate_investment_improved(params):
         'valore_finale_reale': valore_finale_reale,
         'totale_affitti_netti': totale_affitti_netti,
         'totale_affitti_netti_reale': totale_affitti_netti_reale,
+        'totale_affitti_netti_senza_mutuo': totale_affitti_netti_senza_mutuo,
+        'totale_affitti_netti_senza_mutuo_reale': totale_affitti_netti_senza_mutuo_reale,
         'totale_costi_mutuo': totale_costi_mutuo,
         'rendimento_medio_annuo': rendimento_medio_annuo,
         'guadagno_capitale_nominale': guadagno_capitale_nominale,
         'guadagno_capitale_reale': guadagno_capitale_reale,
         'rendimento_totale_nominale': rendimento_totale_nominale,
         'rendimento_totale_reale': rendimento_totale_reale,
+        'rendimento_totale_senza_mutuo_nominale': rendimento_totale_senza_mutuo_nominale,
+        'rendimento_totale_senza_mutuo_reale': rendimento_totale_senza_mutuo_reale,
         'cagr_nominale': cagr_nominale,
         'cagr_reale': cagr_reale,
+        'cagr_senza_mutuo_nominale': cagr_senza_mutuo_nominale,
+        'cagr_senza_mutuo_reale': cagr_senza_mutuo_reale,
         'affitto_finale': affitto_finale,
         'crescita_affitto_totale': crescita_affitto_totale,
         'crescita_affitto_annua': crescita_affitto_annua,
