866595233e21d7b8c41198cd4ca58365e3fb44ac
AlexSart083 <100191834+AlexSart083@users.noreply.github.com>
2025-09-02 09:54:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import streamlit as st
+from i18n import get_text
 
 def setup_page_config():
     st.set_page_config(
-        page_title="Calcolatore Investimenti Immobiliari",
+        page_title=get_text('page_title'),
         page_icon="🏠",
         layout="wide"
     )
