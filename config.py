905b16e7b33ca82b3da678fd370342848f7253ce
AlexSart083 <100191834+AlexSart083@users.noreply.github.com>
2025-09-06 12:22:03 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ import streamlit as st
 from i18n import get_text
 
 def setup_page_config():
+    # Inizializza la lingua di default se non esiste
+    if 'language' not in st.session_state:
+        st.session_state.language = 'it'
+    
     st.set_page_config(
         page_title=get_text('page_title'),
         page_icon="🏠",
